ff0208763e4b6ec1c179b30c3b354a8fd8379433
Git da Bru <bruna.reveriego91@gmail.com>
2021-09-21 21:34:37 -0300

    organizando o codigo em modulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,24 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { PhotoComponent } from './photo/photo.component';
-import { PhotosListComponent } from './photos-list/photos-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photos-list/photos/photos.component';
-import { FilterByDescription } from './photos-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photos-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotosListModule } from './photos-list/photos-list.module';
 
 @NgModule({
-    declarations: [PhotoComponent,
-        PhotosListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
-    imports: [HttpClientModule,
-        CommonModule
+    imports: [
+        PhotoModule,
+        PhotoFormModule,
+        PhotosListModule
     ]
 })
 export class PhotosModule { }
